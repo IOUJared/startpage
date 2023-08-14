51b3691a4d12895b26e3b3087ecdc2555d32c034
IOUJared <22990209+IOUJared@users.noreply.github.com>
2023-08-14 15:30:10 -0500

    Add files via upload

---
+++ bookmarks.js
@@ -0,0 +1,42 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: 'Daily',
+    links: [
+      { name: 'Inbox', url: 'https://inbox.google.com' },
+      { name: 'GitHub', url: 'https://github.com' },
+      { name: 'Drive', url: 'https://drive.google.com' },
+      { name: 'K-News', url: 'https://pannkpop.com/' },
+    ],
+  },
+  {
+    title: 'Media',
+    links: [
+      { name: 'Youtube', url: 'https://youtube.com' },
+      { name: 'Twitch', url: 'https://twitch.tv' },
+      { name: 'Crunchyroll', url: 'https://crunchyroll.com' },
+      { name: 'Animek', url: 'https://animek.fun/' },
+    ],
+  },
+  {
+    title: 'Reddit',
+    links: [
+      { name: '/r/bangtan', url: 'https://reddit.com/r/bangtan' },
+      {
+        name: '/r/kpop',
+        url: 'https://reddit.com/r/kpop',
+      },
+      { name: '/r/popheads', url: 'https://reddit.com/r/popheads' },
+      { name: '/r/LSF', url: 'https://reddit.com/r/livestreamfail' },
+      { name: '/r/news', url: 'https://reddit.com/r/news' },
+    ],
+  },
+  {
+    title: 'Social',
+    links: [
+      { name: 'TweetDeck', url: 'https://tweetdeck.twitter.com/' },
+      { name: 'Twitter', url: 'https://twitter.com' },
+      { name: 'Facebook', url: 'https://facebook.com' },
+    ],
+  },
+];
